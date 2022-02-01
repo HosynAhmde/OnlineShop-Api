bedc25f980c72284951701d8becd8b7367d04c14
Hossein Ahmadi <hosynahmde@gmail.com>
2022-02-01 10:29:31 +0330

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express=require('express');
+const app=express();
+const connectDB=require('./db/conect');
+/////////////routes
+
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+app.use(express.json())
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+      // connectDB
+      await connectDB(process.env.MONGO_URI);
+      app.listen(port, () => console.log(`Server is listening port ${port}...`));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  start()
